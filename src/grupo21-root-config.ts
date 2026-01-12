ff8f223c26914551903315b9eb2e60feff30eaee
paoru5444 <betinho5444@gmail.com>
2026-01-11 22:33:21 -0300

    add root config

---
+++ src/grupo21-root-config.ts
@@ -0,0 +1,21 @@
+import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
+import "./global.css";
+
+const routes = constructRoutes(microfrontendLayout);
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return import(/* webpackIgnore: true */ name);
+  },
+});
+const layoutEngine = constructLayoutEngine({ routes, applications });
+
+applications.forEach(registerApplication);
+layoutEngine.activate();
+start();
